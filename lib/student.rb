7e20c94cd8c2237709ae63618fb4a4c701f4cdd9
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-22 22:50:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,7 +57,7 @@ class Student
     end
   end
 
-  def self.first_x_students_in_grade_10(number)
+  def self.first_x_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * 
       FROM students
@@ -65,7 +65,7 @@ class Student
       LIMIT ?
       SQL
 
-    DB[:conn].execute(sql, number).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end
